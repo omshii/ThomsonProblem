b2e197f437ea2099980a9d58c91b4393d6ae0832
omshii <omshisamal72@gmail.com>
2020-06-29 00:56:09 +0530

    added component for force

--- fast.py
+++ fast.py
@@ -6,6 +6,9 @@ def calc_individual_forces(separations):
     else:
         return np.zeros(3)
 
+def calc_unit_vector(position):
+    return position/np.linalg.norm(position)
+
 particles = np.zeros((3))
 separations = np.zeros((3, 3, 3))
 
@@ -26,6 +29,14 @@ print("Initialized separations:", separations)
 
 individual_forces = np.apply_along_axis(calc_individual_forces, 2, separations)
 total_forces = np.sum(individual_forces, axis=1)
+unit_radii = np.apply_along_axis(calc_unit_vector, 1, positions)
+dot_product = np.transpose(np.einsum('ij, ij->i',total_forces, positions))
+non_component_forces = np.einsum('i, ij->ij', dot_product, unit_radii)
+component_forces = total_forces - non_component_forces
 
 print("individual forces:", individual_forces)
 print("total forces:", total_forces)
+print("unit_radii:", unit_radii)
+print("dot_product", dot_product)
+print("non_component_forces", non_component_forces)
+print("component_forces", component_forces)
