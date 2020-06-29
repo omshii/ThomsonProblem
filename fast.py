15040cd0d80a58edcb62cb759bd12824fb70216b
omshii <omshisamal72@gmail.com>
2020-06-29 15:55:22 +0530

    removed print statements

--- fast.py
+++ fast.py
@@ -26,57 +26,47 @@ print("potential_energy:", '\n', potential_energy)
 
 #Randomized positions
 positions = np.random.rand(3,3)
+positions = np.array([[0.50855717, 0.23848786, 0.83814653], [0.1436721, 0.87508403, 0.04696972], [0.60268039, 0.89345049, 0.02003009]])
+
 print("randomized positions:", '\n', positions)
+positions = np.apply_along_axis(calc_unit_vector, 1, positions)
 
 #Compute separations
 separations = np.einsum('ij, jik->ikj', positions, np.ones_like(separations)) - np.einsum('ij, kij->kij', positions, np.ones_like(separations))
 print("Initialized separations:", '\n', separations)
 
 dt = 0.01
+t = 0
 
-#Calculate forces (without constraint)
-individual_forces = np.apply_along_axis(calc_individual_forces, 2, separations)
-total_forces = np.sum(individual_forces, axis=1)
-print("Unconstrained individual forces:", '\n', individual_forces)
-print("Unconstrained total forces:", '\n', total_forces)
+while t<50:
+    #Calculate forces (without constraint)
+    individual_forces = np.apply_along_axis(calc_individual_forces, 2, separations)
+    total_forces = np.sum(individual_forces, axis=1)
 
-#Calculate constraint for forces
-unit_radii = np.apply_along_axis(calc_unit_vector, 1, positions)
-dot_product = np.einsum('ij, ij->i',total_forces, positions)
-non_component_forces = np.einsum('i, ij->ij', dot_product, unit_radii)
-component_forces = total_forces - non_component_forces
-print("Unit radius:", '\n', unit_radii)
-print("Dot product of forces and positions:", '\n', dot_product)
-print("Non component forces: dp*unit radius vector", '\n', non_component_forces)
-print("component forces: total-nc", '\n', component_forces)
+    #Calculate constraint for forces
+    unit_radii = np.apply_along_axis(calc_unit_vector, 1, positions)
+    dot_product = np.einsum('ij, ij->i',total_forces, positions)
+    non_component_forces = np.einsum('i, ij->ij', dot_product, unit_radii)
+    component_forces = total_forces - non_component_forces
 
-#Calculate and update velocities
-print('\n')
-print('\n')
-print("Unit radius:", '\n', unit_radii)
-velocities = velocities + component_forces*dt
-print("Unconstrained velocities:", '\n', velocities)
-dot_product_velocities = np.einsum('ij, ij->i',velocities, positions)
-non_component_velocties = np.einsum('i, ij->ij', dot_product_velocities, unit_radii)
-velocities = velocities - non_component_velocties
-print("Dot product of velocities and positions:", '\n', dot_product_velocities)
-print("Non component velocities: dp*unit radius vector", '\n', non_component_velocties)
-print("component velocities: total-nc", '\n', velocities)
+    #Calculate and update velocities
+    velocities = velocities + component_forces*dt
+    dot_product_velocities = np.einsum('ij, ij->i',velocities, positions)
+    non_component_velocties = np.einsum('i, ij->ij', dot_product_velocities, unit_radii)
+    velocities = 0.997*(velocities - non_component_velocties)
 
-#Calculate and update positions
-positions = positions + velocities*dt
-print("Unconstrained positions:", '\n', positions)
-positions = np.apply_along_axis(calc_unit_vector, 1, positions)
-print("Constrained positions:", '\n', positions)
+    #Calculate and update positions
+    positions = positions + velocities*dt
+    positions = np.apply_along_axis(calc_unit_vector, 1, positions)
 
-#Calculate and update separations
-separations = np.einsum('ij, jik->ikj', positions, np.ones_like(separations)) - np.einsum('ij, kij->kij', positions, np.ones_like(separations))
-print("Updated separations:", '\n', separations)
+    #Calculate and update separations
+    separations = np.einsum('ij, jik->ikj', positions, np.ones_like(separations)) - np.einsum('ij, kij->kij', positions, np.ones_like(separations))
 
-#Calculate and update potential_energy
-temp_energy = np.apply_along_axis(calc_energy, 2, separations)
-sum = np.sum(temp_energy)
-potential_energy = sum/2
-print("temporary energy:", '\n', temp_energy)
-print("sum:", '\n', sum)
-print("potential_energy:", '\n', potential_energy)
+    #Calculate and update potential_energy
+    temp_energy = np.apply_along_axis(calc_energy, 2, separations)
+    sum = np.sum(temp_energy)
+    potential_energy = sum/2
+
+    print(potential_energy)
+
+    t = t+dt
